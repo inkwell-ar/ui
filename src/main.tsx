baedf6cb8e074659dfb1728048faf6448221bb21
7i7o <mparij@communitylabs.com>
2025-08-20 23:30:03 +0200

    chore: fix filename casing

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { ThemeContextProvider } from "@/contexts/theme-context";
 import { Toaster } from "@/components/ui/sonner";
 import { EnvContextProvider } from "@/contexts/env-context";
 import { WCContextProvider } from "@/contexts/wc-context";
-import { App } from "@/app";
+import { App } from "@/app3";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
