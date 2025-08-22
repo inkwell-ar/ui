8340319e2ec95aa3965e84fd9eb84d987510b4a8
7i7o <mparij@communitylabs.com>
2025-08-22 19:01:27 +0200

    chore: remove comments

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -13,63 +13,6 @@ import { NavBlog } from "./nav-blog";
 import { NavPosts } from "./nav-posts";
 import { NavUsers } from "./nav-users";
 
-// This is sample data.
-// const data = {
-//   user: {
-//     name: "shadcn",
-//     email: "m@example.com",
-//     avatar: "/avatars/shadcn.jpg",
-//   },
-//   blogs: [
-//     {
-//       name: "Announcements",
-//       logo: GalleryVerticalEnd,
-//       description: "Wander",
-//     },
-//     {
-//       name: "Pills",
-//       logo: Command,
-//       description: "Wander",
-//     },
-//     {
-//       name: "Popups",
-//       logo: AudioWaveform,
-//       description: "Wander",
-//     },
-//   ],
-//   blog: [
-//     {
-//       name: "Blog Details",
-//       route: "#",
-//       icon: Settings2,
-//     },
-//   ],
-//   posts: [
-//     {
-//       name: "All Posts",
-//       route: "#",
-//       icon: List,
-//     },
-//     {
-//       name: "Create New Post",
-//       route: "#",
-//       icon: Plus,
-//     },
-//   ],
-//   users: [
-//     {
-//       name: "All Users",
-//       route: "#",
-//       icon: Users2,
-//     },
-//     {
-//       name: "Add User",
-//       route: "#",
-//       icon: UserPlus2,
-//     },
-//   ],
-// };
-
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   const { selectedBlog } = useBlogsContext();
 
@@ -84,11 +27,6 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <NavBlog />
         <NavPosts />
         <NavUsers />
-        {/* {selectedBlog && selectedBlog !== emptyBlogData.id && (
-          <NavCategory name="Blog" items={data.blog} />
-        )}
-        {isEditor && <NavCategory name="Posts" items={data.posts} />}
-        {isAdmin && <NavCategory name="Users" items={data.users} />} */}
       </SidebarContent>
       <SidebarFooter>
         <NavUser />
