ded39cb1550f139cb7260c59182d2a30577d162e
7i7o <mparij@communitylabs.com>
2025-08-23 23:28:35 +0200

    fix: add default route on header logo

--- src/components/nav-header-logo.tsx
+++ src/components/nav-header-logo.tsx
@@ -1,5 +1,6 @@
 import { SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "./ui/sidebar";
 import logo from "@/assets/logo-dark-mode.svg";
+import { Link } from "react-router-dom";
 
 export function NavHeaderLogo() {
   return (
@@ -9,10 +10,10 @@ export function NavHeaderLogo() {
           asChild
           className="data-[slot=sidebar-menu-button]:!p-1.5 h-12 bg-[#e05d38] text-background hover:bg-[#e05d38] hover:text-background dark:text-foreground dark:hover:text-foreground"
         >
-          <a href="#">
+          <Link to="/">
             <img src={logo} alt="🪶" className="!size-8" />
             <span className="text-lg">Inkwell</span>
-          </a>
+          </Link>
         </SidebarMenuButton>
       </SidebarMenuItem>
     </SidebarMenu>
