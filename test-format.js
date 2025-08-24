1f6fd5b8ad1647da2a72523e4402bf7cc015e851
7i7o <mparij@communitylabs.com>
2025-08-24 11:45:03 +0200

    Test pre-commit hook formatting

---
+++ test-format.js
@@ -0,0 +1,14 @@
+// This is a test file with poor formatting
+const test = {
+    foo: 'bar',
+    baz: 123,
+    nested: {
+        value: 'test',
+    },
+};
+
+function badlyFormatted(param) {
+    return param + 'test';
+}
+
+export default test;
